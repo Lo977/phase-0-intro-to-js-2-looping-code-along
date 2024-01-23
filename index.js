031864e3932de35bd4382e12c8c95f17414b5b49
Buddha Lo <lo_977@yahoo.com>
2024-01-23 11:15:46 -0800

    great code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event) {
+  const newMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    newMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return newMessages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+  console.log(number --)
+  }
+}
+countDown(5)
